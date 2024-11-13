ec36747150dcdcbf845da24167ca1fb6af9b2f11
daniel <Danielpachecopz@gmail.com>
2024-11-13 10:56:02 -0500

    providers implementados, y prueba de funcionalidad de https en el homeScreen

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -1,12 +1,27 @@
 import 'package:cine_app/domain/entities/movie.dart';
+import 'package:cine_app/presentation/providers/movies/movie_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nowPlayingMoviesProvider = StateNotifierProvider(
-  (ref) {
-    return MoviesNotifier();
-  },
-);
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPLaying;
+
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
-  MoviesNotifier() : super([]);
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cine_app/config/constants/themoviedbkey.dart';
+import 'package:cine_app/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'Home_screen';
@@ -12,6 +14,38 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('TEXTO GENERICO'),
         ),
-        body: const Placeholder());
+        body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPLayingMovies = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+      itemCount: nowPLayingMovies.length,
+      itemBuilder: (BuildContext context, int index) {
+        final movie = nowPLayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    );
   }
 }
